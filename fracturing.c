8452ceca7449900f4c2ba9fa00fb7de98c16640d
Paul Heffner <pa296550@ucf.edu>
2024-09-04 14:08:16 +0000

    first iteration

---
+++ fracturing.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+#define PI =  3.14159
+
+// first ask for inputs
+
+double askForUserInput(){
+double x1=0;
+double x2=0;
+double y1=0;
+double y2=0;
+
+scanf(" %lf %lf %lf %lf",&x1,&x2,&y1,&y2) ;
+
+}
+
+
+void Baseq() 
+{
+
+
+}
