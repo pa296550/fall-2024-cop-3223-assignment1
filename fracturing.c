73cdfb1d2736a5003168d178e0fd747cdb34d99c
Paul Heffner <pa296550@ucf.edu>
2024-09-04 15:20:08 +0000

    retry 1

--- fracturing.c
+++ fracturing.c
@@ -2,14 +2,36 @@
 #include <stdio.h>
 #define PI =  3.14159
 
-//Follow the pdf step by step
+//Follow the pdf step by step. First lets ask for user input then display the functions all at once. :D
+
+double askForUserInput(void){
+   
+    double x1=0;
+    double x2=0;
+    double y1=0;
+    double y2=0;
+   
+    printf("What is x1?");
+    scanf("%lf", &x1);
+
+    printf("What is x2?");
+    scanf("%lf", &x2);
+
+    printf("What is y1?");
+    scanf("%lf", &y1);
+
+    printf("What is y2?");
+    scanf("%lf", &y2);
+
+    return x1, x2, y1, y2;
+}
 
 int main(int argc, char **argv){
 
-
+//This is where we will call back other functions by simply stating the function name
+double askForUserInput();
     return 0;
 }
-
 
 
 
