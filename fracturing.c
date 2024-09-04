ba2fe4ff8cbf05a032a7e1a85bb0f35cb08ef2a7
Paul Heffner <pa296550@ucf.edu>
2024-09-04 14:18:18 +0000

    2nd

--- fracturing.c
+++ fracturing.c
@@ -4,6 +4,7 @@
 
 // first ask for inputs
 
+
 double askForUserInput(){
 double x1=0;
 double x2=0;
@@ -12,11 +13,18 @@ double y2=0;
 
 scanf(" %lf %lf %lf %lf",&x1,&x2,&y1,&y2) ;
 
+return x1, x2, y1, y2; 
 }
 
 
 void Baseq() 
 {
 
+
+}
+
+double main() {
+
+printf("\n %lf \n %lf \n %lf \n %lf", x1, x2, y1, y2);
 
 }
