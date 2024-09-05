eeff2f51aa395abf51ea5a414cb952c7ebe7acdd
Paul Heffner <pa296550@ucf.edu>
2024-09-05 17:30:12 +0000

    Trying something new

--- fracturing.c
+++ fracturing.c
@@ -4,32 +4,62 @@
 
 //Follow the pdf step by step. First lets ask for user input then display the functions all at once. :D
 
-double askForUserInput(void){
-   
-    double x1=0;
-    double x2=0;
-    double y1=0;
-    double y2=0;
-   
-    printf("What is x1?");
+
+void AskForx1() {
+    
+    double x1=0 ;
+    
+    printf("What is x1?\n");
+    
     scanf("%lf", &x1);
 
-    printf("What is x2?");
+    
+}
+
+void AskForx2() {
+    
+    double x2=0;
+    
+    printf("What is x2?\n");
     scanf("%lf", &x2);
 
-    printf("What is y1?");
+    
+}
+
+void AskFory1(){
+
+    double y1=0;
+    
+    printf("What is y1?\n");
     scanf("%lf", &y1);
 
-    printf("What is y2?");
-    scanf("%lf", &y2);
+    
 
-    return x1, x2, y1, y2;
 }
+
+void AskFory2 (){
+
+    double y2=0;
+
+    printf("What is y2?\n");
+    scanf("%lf", &y2);
+    
+    
+
+}
+
 
 int main(int argc, char **argv){
 
 //This is where we will call back other functions by simply stating the function name
-double askForUserInput();
+    
+    AskForx1(); 
+    AskForx2();
+    AskFory1();
+    AskFory2();
+    
+    printf("%lf", 1);
+
     return 0;
 }
 
