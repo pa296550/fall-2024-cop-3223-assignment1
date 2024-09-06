411669f5f336675c88196e29065df4560f5ce46b
Paul Heffner <pa296550@ucf.edu>
2024-09-06 16:19:43 +0000

    try to ask 5 times instead of once

---
+++ Old.c
@@ -0,0 +1,83 @@
+#include <math.h>
+#include <stdio.h>
+#define PI =  3.14159
+
+//Follow the pdf step by step. First lets ask for user input then display the functions all at once. :D
+
+
+void AskForx1(double val) {
+    double x1;
+    
+
+
+    printf("What is x1?\n");
+    
+    scanf("%lf", &x1);
+
+    x1= val 
+    // try to use val? 
+    return; 
+}
+
+double AskForx2() {
+    
+    double x2=0;
+    
+    printf("What is x2?\n");
+    scanf("%lf", &x2);
+
+    return x2;
+}
+
+double AskFory1(){
+
+    double y1=0;
+    
+    printf("What is y1?\n");
+    scanf("%lf", &y1);
+
+    return y1;
+    
+
+}
+
+double AskFory2 (){
+
+    double y2=0;
+
+    printf("What is y2?\n");
+    scanf("%lf", &y2);
+    
+    return y2;
+
+}
+
+double calculateDistance(){
+
+    double x1 = AskForx1(); 
+    double x2 = AskForx2(); 
+    double y1= AskFory1();
+    double y2= AskFory2();
+
+    double DifferenceSum = (x2-x1) * (x2-x1) + (y2-y1)*(y2-y1);
+
+    double Distance = sqrt(DifferenceSum);
+    printf("%lf", Distance);
+
+    return Distance;
+
+}
+
+int main(int argc, char **argv){
+
+//This is where we will call back other functions by simply stating the function name
+   double calculateDistance();
+
+    
+   
+
+    return 0;
+}
+
+
+
--- fracturing.c
+++ fracturing.c
@@ -3,77 +3,37 @@
 #define PI =  3.14159
 
 //Follow the pdf step by step. First lets ask for user input then display the functions all at once. :D
+//haha just kidding just copy and past this code each time to ask for inputs
 
+// double askForUserInput(){
+   
+//     double x1=0;
+//     double x2=0;
+//     double y1=0;
+//     double y2=0;
+   
+//     printf("What is x1?");
+//     scanf("%lf", &x1);
 
-double AskForx1() {
-    
-    double x1=0;
-    
-    printf("What is x1?\n");
-    
-    scanf("%lf", &x1);
-    return x1; 
-}
+//     printf("What is x2?");
+//     scanf("%lf", &x2);
 
-double AskForx2() {
-    
-    double x2=0;
-    
-    printf("What is x2?\n");
-    scanf("%lf", &x2);
+//     printf("What is y1?");
+//     scanf("%lf", &y1);
 
-    return x2;
-}
+//     printf("What is y2?");
+//     scanf("%lf", &y2);
 
-double AskFory1(){
-
-    double y1=0;
-    
-    printf("What is y1?\n");
-    scanf("%lf", &y1);
-
-    return y1;
-    
-
-}
-
-double AskFory2 (){
-
-    double y2=0;
-
-    printf("What is y2?\n");
-    scanf("%lf", &y2);
-    
-    return y2;
-
-}
-
-double calculateDistance(){
-
-    double x1 = AskForx1(); 
-    double x2 = AskForx2(); 
-    double y1= AskFory1();
-    double y2= AskFory2();
-
-    double DifferenceSum = (x2-x1) * (x2-x1) + (y2-y1)*(y2-y1);
-
-    double Distance = sqrt(DifferenceSum);
-    printf("%lf", Distance);
-
-    return Distance;
-
-}
+//     return 0;
+// }
 
 int main(int argc, char **argv){
-
+d
 //This is where we will call back other functions by simply stating the function name
-   double calculateDistance();
+askForUserInput();
 
-    
-   
+
+
 
     return 0;
 }
-
-
-
