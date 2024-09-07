3d8866a9b18b229c4445cd7950a42bf69b38c5fa
Paul Heffner <pa296550@ucf.edu>
2024-09-07 14:43:09 +0000

    FINAL!!!

--- fracturing.c
+++ fracturing.c
@@ -1,11 +1,10 @@
 #include <math.h>
 #include <stdio.h>
-#define PI =  3.14159
-
+#define PI 3.14159
 //Follow the pdf step by step. First lets ask for user input then display the functions all at once. :D
-//haha just kidding just copy and past this code each time to ask for inputs
+//haha just kidding just copy and past this code each time to ask for inputs (I made this)
 
-// double askForUserInput(){
+// double askForUserInput(){   <--- I had trouble trying to implement this into my code, So i resorted to just asking each time
    
 //     double x1=0;
 //     double x2=0;
@@ -27,7 +26,17 @@
 //     return 0;
 // }
 
-double calculateDistance(){
+double distancer(double Ax,double Bx,double Cx,double Dx){
+
+double Adding = pow(Dx-Cx, 2) + pow(Bx-Ax, 2);
+double distance = sqrt(Adding);
+
+return distance;
+
+}
+    
+
+double calculateDistance(){  // calculates distance between the two points
     double x1=0;
     double x2=0;
     double y1=0;
@@ -45,24 +54,22 @@ double calculateDistance(){
     printf("What is y2?");
     scanf("%lf", &y2);
 
-    double Adding = pow(y1-y2, 2) + pow(x1-x2, 2);
     
-    double Distance = sqrt(Adding);
+    double Distance = distancer(x1,x2,y1,y2);
     printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf\n– The distance between the two points is %.3lf\n", x1,y1,x2,y2, Distance);
 
     return  Distance ;
 }
 
 
-double calculatePerimeter(){
 
-    
+double calculatePerimeter(){ //calculates the perimeter 
 
     double x1=0;
     double x2=0;
     double y1=0;
     double y2=0;
-   
+
     printf("Time to calculate Perimeter! \nWhat is x1?");
     scanf("%lf", &x1);
 
@@ -75,19 +82,17 @@ double calculatePerimeter(){
     printf("What is y2?");
     scanf("%lf", &y2);
 
-    double vertical = (y2 - y1) ;
-    double horizontal = (x2 - x1);
+// Perimeter of a circle is PI * Diameter
 
-    double Perimeter = 2 * fabs(vertical) + 2 * fabs(horizontal);
+    double Perimeter = (PI) * (distancer(x1,x2,y1,y2));
 
-printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf- \n– The perimeter of the city encompassed by your request is %.3lf\n", x1,y1,x2,y2, Perimeter);
+    printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf; \n– The perimeter of the city encompassed by your request is %.3lf\n", x1,y1,x2,y2, Perimeter);
 
-
-    double difficulty = 1;
+    double difficulty = 2;
     return difficulty ;
 }
 
-double calculateArea(){
+double calculateArea(){ //calculates the area 
 
 
     double x1=0;
@@ -107,17 +112,20 @@ double calculateArea(){
     printf("What is y2?");
     scanf("%lf", &y2);
 
-    double Area = (y2-y1) * (x2-x1); 
+// Area of a circle is PI (D/2)^2
 
-    printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf-\n– The area of the city encompassed by your request is %.3lf\n", x1,y1,x2,y2, Area);
+    double Area1 = (distancer(x1,x2,y1,y2)/2);
+    double Area = PI * pow(Area1, 2);
 
-    double difficulty = 1;
+    printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf;\n– The area of the city encompassed by your request is %.3lf\n", x1,y1,x2,y2, Area);
+
+    double difficulty = 2;
 
     return difficulty; 
 
 }
 
-double calculateWidth(){
+double calculateWidth(){ // calculates the width assuming that the x axis represents width
 
     double x1=0;
     double x2=0;
@@ -138,7 +146,7 @@ double calculateWidth(){
 
     double Width = fabs(x2-x1);
 
-    printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf- \n– The width of the city encompassed by your request is %.3lf\n", x1,y1,x2,y2, Width);
+    printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf; \n– The width of the city encompassed by your request is %.3lf\n", x1,y1,x2,y2, Width);
 
     double Difficulty = 1;
     
@@ -146,7 +154,7 @@ double calculateWidth(){
 
 }
 
-double calculateHeight(){
+double calculateHeight(){ //calculates height assuming the y axis represents height
 
     double x1=0;
     double x2=0;
@@ -167,7 +175,7 @@ double calculateHeight(){
 
     double Width = fabs(y2-y1);
 
-    printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf-\n– The height of the city encompassed by your request is %.3lf", x1,y1,x2,y2, Width);
+    printf( "– Point #1 entered: x1 = %.3lf; y1 = %.3lf;\n– Point #2 entered: x2 = %.3lf; y2 = %.3lf;\n– The height of the city encompassed by your request is %.3lf", x1,y1,x2,y2, Width);
 
     double Difficulty = 1;
 
@@ -179,7 +187,6 @@ double calculateHeight(){
 int main(int argc, char **argv){
 
 //This is where we will call back other functions by simply stating the function name
-
 calculateDistance();
 calculatePerimeter();
 calculateArea();
